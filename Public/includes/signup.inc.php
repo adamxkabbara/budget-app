47e9c17896c4d7a5097fe35ba1c8b536e5f76de4
Adam Kabbara <adamxkabbara@gmail.com>
2020-04-02 20:18:57 -0700

    fix uri

--- Public/includes/signup.inc.php
+++ Public/includes/signup.inc.php
@@ -3,8 +3,9 @@
   if (isset($_POST["signup-submit"])) {
 
     // include files
-    include_once(__DIR__ . '/../../Controllers/UserController.php');
-    include_once(__DIR__ . '/../../models/User.class.php');
+    include_once __DIR__ . '/../../Controllers/UserController.php';
+    include_once __DIR__ . '/../../models/User.class.php';
+    include_once __DIR__ . '/../../Utils/Constants.inc.php';
 
     $username = $_POST["uid"]; 
     $email = $_POST["email"];
@@ -14,32 +15,32 @@
     // User input check
     if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
 
-      header("Location: ../views/signup.php?error=emptyfields&uid=$username&mail=$email");
+      header("Location: /signup?error=emptyfields&uid=$username&mail=$email");
       exit();
     }
     else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
 
-      header("Location: ../views/signup.php?error=invalidmailuid");
+      header("Location: /signup?error=invalidmailuid");
       exit();
     }
     else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-      header("Location: ../views/signup.php?error=invalidmail&uid=$username");
+      header("Location: /signup?error=invalidmail&uid=$username");
       exit();
     }
     else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
 
-      header("Location: ../views/signup.php?error=invaliduid&mail=$email");
+      header("Location: /signup?error=invaliduid&mail=$email");
       exit();
     }
     else if (!preg_match('/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9]{8,}$/', $password)) {
 
-      header("Location: ../views/signup.php?error=passwordmalformed&uid=$username&mail=$email");
+      header("Location: /signup?error=passwordmalformed&uid=$username&mail=$email");
       exit();
     }
     else if ($password !== $passwordRepeat) {
 
-      header("Location: ../views/signup.php?error=passwordcheck&uid=$username&mail=$email");
+      header("Location: /signup?error=passwordcheck&uid=$username&mail=$email");
       exit();
     }
     else {
@@ -49,23 +50,26 @@
       $user_controller = new UserController();
 
       $result = $user_controller->create($user_obj);
-      if ($result === 1062) {
-        //User taken
-
-        header("Location: ../views/signup.php?error=userTaken&mail=$email");
+      if ($result === SUCCESS) {
+        header("Location: /dashboard?signup=success");
         exit();
       }
-      else if ($result === 0) {
-        header("Location: ../views/signup.php?signup=success");
+      else if ($result === ER_DUP_ENTRY) {
+        //User taken
+        header("Location: /signup?error=userTaken&mail=$email");
+        exit();
+      }
+      else if ($result === ER_PARSE_ERROR) {
+        header("Location: /signup?error=invalidsql");
         exit();
       }
       else {
-        header("Location: ../views/signup.php?error=invalidsql");
+        header("Location: /signup.php?error=invalid");
         exit();
       }
     }
   }
   else {
-      header("Location: ../signup.phpp");
-      exit();
+    header("Location: /signup");
+    exit();
   }
--- Public/views/header.php
+++ Public/views/header.php
@@ -2,10 +2,10 @@
   <?php
   if (isset($_SESSION['userUid'])) {
     // User logged in
-    echo '<a href="/views/index.php">Dashboard</a>
+    echo '<a href="/dashboard">Dashboard</a>
           <form class="main-button" action="../../includes/logout.inc.php" method="post">
-                  <input type="submit" name="logout-submit" value="Logout">
-                </form>';
+            <input type="submit" name="logout-submit" value="Logout">
+          </form>';
   } else {
     // NOT logged in
   }
