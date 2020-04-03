42ffce5cfac98eedf0bc9c02baacda42a4ee1e67
Adam Kabbara <adamxkabbara@gmail.com>
2020-04-02 18:49:30 -0700

    rename files

--- Public/views/index.php
+++ Public/views/index.php
@@ -1,37 +1,11 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
-  <link rel="preload" href="../fonts/regular.otf" as="font" type="font/otf" crossorigin>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="data:,">
-  <link type="text/css" rel="stylesheet" href="../styles/styles.css">
+  <title>Budget-App</title>
 </head>
-
 <body>
-  <?php
-  require './header.php';
-  ?>
-
-  <script src="../scripts/index.js"></script>
-
-  <?php
-  if (isset($_SESSION['userUid'])) {
-    echo '<h1>' . $_SESSION['userUid'] . '</h1>';
-    echo '<div id=getTime></div>';
-  } else {
-    echo '<h1>' . $_SESSION['userUid'] . '</h1>';
-    echo '<h1>NOT SIGNED IN</h1>';
-  }
-  ?>
-
-  <?php
-  require './footer.php';
-  ?>
+  <h1>budget-app is here to help you pilot your finances</h1>
 </body>
-
 </html>
