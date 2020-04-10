35cef2b17f4b40d9dd19aeeaed6c1aa927f16d31
velin13 <velin@ucsd.edu>
2020-04-10 15:43:03 -0700

    Update navbar icons

--- Public/views/header.php
+++ Public/views/header.php
@@ -8,13 +8,13 @@
     // User logged in
     echo '<ul class="menu">
           <label id="close">&#10006;</label>
-          <a class="nav-link" href="/dashboard"><img src=\'../media/trend.svg\' width=30 height=35/>Profile</a>
-          <a class="nav-link" href="/dashboard"><img src=\'../media/trend.svg\' width=30 height=35/>Dashboard</a>
-          <a class="nav-link" href="/transactions"><img src=\'../media/transaction.svg\' width=30 height=35/>Transactions</a>
+          <a class="nav-link" href="/dashboard"><img src=\'../media/profile.svg\' width=30 height=35/>Profile</a>
+          <a class="nav-link" href="/dashboard"><img src=\'../media/dashboard.svg\' width=30 height=35/>Dashboard</a>
+          <a class="nav-link" href="/transactions"><img src=\'../media/transactions.svg\' width=30 height=35/>Transactions</a>
           <a class="nav-link" href="/trends"><img src=\'../media/trend.svg\' width=30 height=35/>Trends</a>
-          <a class="nav-link" href="/transactions"><img src=\'../media/transaction.svg\' width=30 height=35/>Settings</a>
+          <a class="nav-link" href="/transactions"><img src=\'../media/settings.svg\' width=30 height=35/>Settings</a>
           <form class="main-button" action="../../includes/logout.inc.php" method="post">
-          <img src=\'../media/logout.svg\' width=30 height=30/><input type="submit" name="logout-submit" value="Logout">
+          <img src=\'../media/logout.svg\' width=30 height=35/><input type="submit" name="logout-submit" value="Logout">
           </form>
           </ul>
           
