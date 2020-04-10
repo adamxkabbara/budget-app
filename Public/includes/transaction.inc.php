9306a4b472a7ad7553a93932ac7d65f20beb4a76
Adam Kabbara <adamxkabbara@gmail.com>
2020-04-10 01:33:38 -0700

    update

--- Public/includes/transaction.inc.php
+++ Public/includes/transaction.inc.php
@@ -22,7 +22,7 @@
       
             $userId = $_SESSION['userUid'];
             $date = date("Y-m-d H:i:s");
-            $expense_obj = new Expense(null, $userID, null, $merchant, $amount, $category, $notes, $date, null);
+            $expense_obj = new Expense(null, $userId, null, $merchant, $amount, $category, $notes, $date, null);
             $expense_controller = new ExpenseController();
       
             $result = $expense_controller->create($expense_obj);
